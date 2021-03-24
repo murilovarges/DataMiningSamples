ae4086e1deebbe4117d605dfa2908de0903066a0
Murilo Varges <murilo.varges@gmail.com>
2021-03-24 11:34:59 -0300

    Implemented code for data reduction using PCA.

--- 1-Preprocessing/DataCleaning.py
+++ 1-Preprocessing/DataCleaning.py
@@ -3,69 +3,76 @@ import numpy as np
 
 def main():
     # Faz a leitura do arquivo
+    names = ['BI-RADS','Age','Shape','Margin','Density','Severity'] 
+    features = ['Age','Shape','Margin','Density','Severity']
+    output_file = '0-Datasets/MamoClear.data'
     input_file = '0-Datasets/Mamo.data'
     df = pd.read_csv(input_file, # Nome do arquivo com dados
-                     names =['BI-RADS','Age','Shape','Margin','Density','Severity'], # Nome das colunas 
-                     usecols = ['Age','Shape','Margin','Density','Severity'], # Define as colunas que serão  utilizadas
+                     names = names, # Nome das colunas 
+                     usecols = features, # Define as colunas que serão  utilizadas
                      na_values='?') # Define que ? será considerado valores ausentes
     
+    df_original = df.copy()
     # Imprime as 15 primeiras linhas do arquivo
     print("PRIMEIRAS 15 LINHAS\n")
     print(df.head(15))
-    print("\n")
+    print("\n")        
 
     # Imprime informações sobre dos dados
     print("INFORMAÇÕES GERAIS DOS DADOS\n")
     print(df.info())
     print("\n")
-
+    
     # Imprime uma analise descritiva sobre dos dados
     print("DESCRIÇÃO DOS DADOS\n")
     print(df.describe())
     print("\n")
-
+    
     # Imprime a quantidade de valores faltantes por coluna
     print("VALORES FALTANTES\n")
     print(df.isnull().sum())
     print("\n")
-
-
+    
     # Tratando valores faltantes da coluna Density
     print("VALORES FALTANTES DA COLUNA Density\n")
     print('Total valores ausentes: ' + str(df['Density'].isnull().sum()))
 
-    method = 'mode' # number or median or mean or mode
-
-    if method == 'number':
-        # Substituindo valores ausentes por um número
-        df['Density'].fillna(125, inplace=True)
-
-        # Substituindo valores de linhas específicas por um numero
-        df.loc[2,'Density'] = 125
-
-    elif method == 'median':
-        # Substituindo valores ausentes pela mediana 
-        median = df['Density'].median()
-        df['Density'].fillna(median, inplace=True)
-
-    elif method == 'mean':
-        # Substituindo valores ausentes pela média
-        mean = df['Density'].mean()
-        df['Density'].fillna(mean, inplace=True)    
-      
-    elif method == 'mode':
-        # Substituindo valores ausentes pela moda
-        mode = df['Density'].mode()[0]
-        print(mode)
-        df['Density'].fillna(mode, inplace=True)    
     
+    columns_missing_value = df.columns[df.isnull().any()]
+    print(columns_missing_value)
+    method = 'mode' # number or median or mean or mode
+    
+    for c in columns_missing_value:
+        UptateMissingvalue(df, c)
     
     print('Total valores ausentes: ' + str(df['Density'].isnull().sum()))
     print(df.describe())
     print("\n")
-
+    print(df.head(15))
+    print(df_original.head(15))
     print("\n")
     
+    # Salva arquivo com o tratamento para dados faltantes
+    df.to_csv(output_file, header=False, index=False)  
+    
+
+def UptateMissingvalue(df, column, method="mode", number=0):
+    if method == 'number':
+        # Substituindo valores ausentes por um número
+        df[column].fillna(number, inplace=True)
+    elif method == 'median':
+        # Substituindo valores ausentes pela mediana 
+        median = df['Density'].median()
+        df[column].fillna(median, inplace=True)
+    elif method == 'mean':
+        # Substituindo valores ausentes pela média
+        mean = df[column].mean()
+        df[column].fillna(mean, inplace=True)
+    elif method == 'mode':
+        # Substituindo valores ausentes pela moda
+        mode = df[column].mode()[0]
+        df[column].fillna(mode, inplace=True)
+
 
 if __name__ == "__main__":
     main()
---
+++ 1-Preprocessing/DataReduction.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/MamoClear.data'
+    names = ['Age','Shape','Margin','Density','Severity']
+    features = ['Age','Shape','Margin','Density']
+    target = 'Severity'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names) # Nome das colunas                      
+    ShowInformationDataFrame(df,"Dataframe original")
+
+    # Separating out the features
+    x = df.loc[:, features].values
+    
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+    normalizedDf = pd.DataFrame(data = x, columns = features)
+    normalizedDf = pd.concat([normalizedDf, df[[target]]], axis = 1)
+    ShowInformationDataFrame(normalizedDf,"Dataframe Normalized")
+
+    # PCA projection
+    pca = PCA()    
+    principalComponents = pca.fit_transform(x)
+    print("Explained variance per component:")
+    print(pca.explained_variance_ratio_.tolist())
+    print("\n\n")
+
+    principalDf = pd.DataFrame(data = principalComponents[:,0:2], 
+                               columns = ['principal component 1', 
+                                          'principal component 2'])
+    finalDf = pd.concat([principalDf, df[[target]]], axis = 1)    
+    ShowInformationDataFrame(finalDf,"Dataframe PCA")
+    
+    VisualizePcaProjection(finalDf, target)
+
+
+def ShowInformationDataFrame(df, message=""):
+    print(message+"\n")
+    print(df.info())
+    print(df.describe())
+    print(df.head(10))
+    print("\n")
+    
+           
+def VisualizePcaProjection(finalDf, targetColumn):
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA', fontsize = 20)
+    targets = [0, 1, ]
+    colors = ['r', 'g']
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDf[targetColumn] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   c = color, s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

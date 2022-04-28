2e9c8c51e6c8728cd73cc398b8781ef2335ce76c
Murilo <murilo.varges@gmail.com>
2022-04-28 16:13:35 -0300

    Implementação Decision Tree.

---
+++ 3-Classification/DecisionTree.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+def main():
+    iris = load_iris()
+    print(iris.data)
+    print(iris.target)
+    X = iris.data
+    y = iris.target
+
+    # Split the data - 75% train, 25% test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)  # Load iris data and store in dataframe
+    iris = datasets.load_iris()
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    df['target'] = iris.target
+    df.head()
+
+    # Separate X and y data
+    X = df.drop('target', axis=1)
+    y = df.target   
+    print("Total samples: {}".format(X.shape[0]))
+
+    # Split the data - 75% train, 25% test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)    
+    iris = load_iris()
+    print(iris.data)
+    print(iris.target)
+    X = iris.data
+    y = iris.target
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    clf = DecisionTreeClassifier(max_leaf_nodes=3)
+    clf.fit(X_train, y_train)
+    tree.plot_tree(clf)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 3-Classification/DecisionTreeMamo.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def main():
+    input_file = '0-Datasets/MamoClear.data'
+    names = ['Age','Shape','Margin','Density','Severity']
+    features = ['Age','Shape','Margin','Density']
+    target = 'Severity'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names) # Nome das colunas                      
+   
+    # Separating out the features
+    X = df.loc[:, features].values
+
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Standardizing the features
+    X = StandardScaler().fit_transform(X)
+    normalizedDf = pd.DataFrame(data = X, columns = features)
+    normalizedDf = pd.concat([normalizedDf, df[[target]]], axis = 1)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    clf = DecisionTreeClassifier(max_leaf_nodes=3)
+    clf.fit(X_train, y_train)
+    tree.plot_tree(clf)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
